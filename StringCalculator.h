e4c1a9cfbd03f81e3b90f5d2f2bd8ee1c10e21ae
MAHAN <83449707+mahanthas@users.noreply.github.com>
2024-07-18 13:45:49 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,63 +1,75 @@
-#ifndef STRINGCALCULATOR_H
-#define STRINGCALCULATOR_H
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include <stdbool.h>
-#include <limits.h>
-
-#define MAX_INPUT_LENGTH 1024
-#define DELIMITER_MAX_LENGTH 10
-
-int add(const char* numbers) {
-    if (numbers == NULL || *numbers == '\0') {
-        return 0;
-    }
-
-    char delimiters[DELIMITER_MAX_LENGTH] = ",\n";
-    char numStr[MAX_INPUT_LENGTH];
-    strncpy(numStr, numbers, MAX_INPUT_LENGTH);
-
-    // Handle custom delimiter
-    if (numStr[0] == '/' && numStr[1] == '/') {
-        char* newline_pos = strchr(numStr, '\n');
-        if (newline_pos != NULL) {
-            size_t delimiter_length = newline_pos - (numStr + 2);
-            if (delimiter_length < DELIMITER_MAX_LENGTH) {
-                strncpy(delimiters, numStr + 2, delimiter_length);
-                delimiters[delimiter_length] = '\0';
-            }
-            memmove(numStr, newline_pos + 1, strlen(newline_pos));
-        }
-    }
-
-    int sum = 0;
-    char* token = strtok(numStr, delimiters);
-    bool has_negative = false;
-    char negative_numbers[MAX_INPUT_LENGTH] = "";
-
-    while (token != NULL) {
-        int number = atoi(token);
-
-        if (number < 0) {
-            has_negative = true;
-            char buffer[20];
-            snprintf(buffer, sizeof(buffer), "%d ", number);
-            strncat(negative_numbers, buffer, sizeof(negative_numbers) - strlen(negative_numbers) - 1);
-        } else if (number <= 1000) {
-            sum += number;
-        }
-
+ 
+bool detect_custom_delimiter(const char* str) {
+    return str[0] == '/' && str[1] == '/';
+}
+ 
+void get_custom_delimiter(const char* str, char* delimiter) {
+    const char* start = str + 2; // Skip over "//"
+    size_t length = strcspn(start, "\n"); // Find the position of the newline character
+    strncpy(delimiter, start, length); // Copy the delimiter
+    delimiter[length] = '\0'; // Null-terminate the delimiter
+}
+ 
+void tokenize_numbers(const char* str, const char* delimiters, int* nums, int* num_count) {
+    char* copy = strdup(str);
+    char* token = strtok(copy, delimiters);
+    while (token) {
+        nums[(*num_count)++] = atoi(token);
         token = strtok(NULL, delimiters);
     }
-
-    if (has_negative) {
-        fprintf(stderr, "Negatives not allowed: %s\n", negative_numbers);
-        exit(EXIT_FAILURE);
-    }
-
-    return sum;
+    free(copy);
 }
-
-#endif // STRINGCALCULATOR_H
+ 
+bool find_negatives(int* nums, int num_count, char* error_msg) {
+    bool has_negatives = false;
+    strcpy(error_msg, "negatives not allowed: ");
+    for (int i = 0; i < num_count; i++) {
+        if (nums[i] < 0) {
+            has_negatives = true;
+            char num_str[12];
+            snprintf(num_str, sizeof(num_str), "%d ", nums[i]);
+            strcat(error_msg, num_str);
+        }
+    }
+    return has_negatives;
+}
+ 
+int sum_numbers(int* nums, int num_count) {
+    int total = 0;
+    for (int i = 0; i < num_count; i++) {
+        if (nums[i] <= 1000) {
+            total += nums[i];
+        }
+    }
+    return total;
+}
+ 
+int process_and_sum(const char* str, const char* delimiters) {
+    int nums[1000];
+    int num_count = 0;
+    tokenize_numbers(str, delimiters, nums, &num_count);
+ 
+    char error_msg[256];
+    if (find_negatives(nums, num_count, error_msg)) {
+        return -1; // Indicate an error
+    }
+ 
+    return sum_numbers(nums, num_count);
+}
+ 
+int add(const char* str) {
+    if (*str == '\0') {
+        return 0;
+    }
+ 
+    char delimiter[10] = ",\n";
+    if (detect_custom_delimiter(str)) {
+        get_custom_delimiter(str, delimiter);
+    }
+ 
+    return process_and_sum(str, delimiter);
+}
